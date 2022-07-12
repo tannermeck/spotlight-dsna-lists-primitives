20b13dba6cac90139f1ba5a67a7c235cf7c16e3a
TANNER MECK <steve.meck246@gmail.com>
2022-07-12 13:47:59 -0600

    3 tests passing

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-function reverseWords(sentence) {}
-
-module.exports = { reverseWords };
+function reverseWords(sentence) {
+  const letters = sentence.split(" ");
+  return letters.map((word) => word.split("").reverse().join("")).join(" ");
+}
+function titleCase(sentence) {
+  return sentence
+    .split(" ")
+    .map((word) => word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase())
+    .join(" ");
+}
+function oddishOrEvenish(number) {
+  const nums = number.split("");
+  // [1, 2, 1]
+  let total = 0;
+  nums.map((num) => (total += +num));
+  console.log(total);
+  if (total % 2 > 0) {
+    return "Oddish";
+  } else {
+    return "Evenish";
+  }
+}
+module.exports = { reverseWords, titleCase, oddishOrEvenish };
--- index.test.js
+++ index.test.js
@@ -1,7 +1,14 @@
-const { reverseWords } = require(".");
+const { reverseWords, titleCase, oddishOrEvenish } = require(".");
 
 describe("reverseWords", () => {
   it("should reverse the words of a sentence", () => {
     expect(reverseWords("alchemy rocks gold")).toBe("ymehcla skcor dlog");
+  });
+  it("should capitalize the first letter of each word", () => {
+    expect(titleCase("alchemy ROCKS goLD")).toBe("Alchemy Rocks Gold");
+  });
+  it("return odd or even", () => {
+    expect(oddishOrEvenish("121")).toBe("Evenish");
+    expect(oddishOrEvenish("41")).toBe("Oddish");
   });
 });
